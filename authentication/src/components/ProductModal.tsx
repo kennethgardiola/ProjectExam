5876eae5949e342a62bd240995f5070d75e6dfc4
kennethgardiola <99859424+kennethgardiola@users.noreply.github.com>
2024-12-11 11:06:37 +0800

    ProductModal adjustment

--- authentication/src/components/ProductModal.tsx
+++ authentication/src/components/ProductModal.tsx
@@ -101,14 +101,16 @@ const ProductModal = () => {
                   >
                     Age
                   </label>
-                  <textarea
+                  <input
                     onChange={handleInput}
                     value={discussion.Age}
+                    type="number"
                     id="Age"
                     className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Input age"
+                    min="0"
                     required
-                  ></textarea>
+                  />
                 </div>
                 <div>
                   <label
